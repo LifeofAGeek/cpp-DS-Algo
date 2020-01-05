106c48bd7eaf121238fbb6c88f1eea459d39d8cd
LifeofAGeek <sethi8001@gmail.com>
2020-01-06 00:20:03 +0530

    dynamic size inside Heap Mem.

---
+++ HeapMem.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int size;
+    cout<<"Enter number of elements"<<endl;
+    cin>>size;
+    int A[size];
+
+    cout<<sizeof A<<endl;
+    return 0;
+}
