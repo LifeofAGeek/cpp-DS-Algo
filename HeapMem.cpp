e4b8b6eee72f67963de00ff2d019bf4015d7ea20
LifeofAGeek <sethi8001@gmail.com>
2020-01-06 00:26:29 +0530

    dynamic size inside Heap Mem.

--- HeapMem.cpp
+++ HeapMem.cpp
@@ -8,5 +8,15 @@ int main()
     int A[size];
 
     cout<<sizeof A<<endl;
+
+    int *p=new int[size];
+
+    delete p;
+
+    int newsize;
+    cout<<"Enter new size"<<endl;
+    cin>>newsize;
+
+    p=new int[newsize];
     return 0;
 }
