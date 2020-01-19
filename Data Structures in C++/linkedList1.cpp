d313031ec774b3206e762549de0389d974e05d4e
LifeofAGeek <sethi8001@gmail.com>
2020-01-20 02:15:11 +0530

    Inserting a node in the beginning of linked list

--- Data Structures in C++/linkedList1.cpp
+++ Data Structures in C++/linkedList1.cpp
@@ -7,11 +7,43 @@ struct Node
     int data;
     Node *link;
 };
-struct Node *head; //global variable, can be accessed anywhere
-int Insert(int x);
-void Print();
+Node *head; //global variable, can be accessed anywhere
+
+int Insert(int x) //Insert data in a node
+{
+    Node *temp=new node;
+    temp->data=x;
+    temp->link=head;
+    if(head!=nullptr)
+        temp->link=head;
+    head=temp;
+}
+
+void Print()
+{
+    Node *temp=head;
+    cout<<
+    while(temp!=nullptr)
+    {
+        cout<<temp->data;
+        temp=temp->link;
+
+    }
+}
 int main()
 {
-    head=nullptr;
-    cout<<"all good";
+    head=nullptr; // empty list
+    cout<<"How many nodes?"<<endl;
+    int n,i,x;
+    cin>>n;
+
+    for(i=0;i<n;i++)
+    {
+        cout<<"Enter data of node:"<<i<<endl;
+        cin>>x;
+        Insert(x);
+        Print();
+    }
+
+
 }
