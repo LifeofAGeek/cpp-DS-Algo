c3886ed8b3d1c90c255059d3ac495ecef3764496
LifeofAGeek <sethi8001@gmail.com>
2020-02-01 23:13:13 +0530

    basic oops

--- oops1.cpp
+++ oops1.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
 
+class Rectangle
+{
+public:
+
+    float length;
+    float breadth;
+
+    float area()
+    {
+        return length*breadth;
+    }
+
+    float perimeter()
+    {
+        return 2*(length+breadth);
+    }
+};
+
+int main()
+{
+    Rectangle r1,r2;
+    r1.length=10;
+    r1.breadth=5;
+    cout<<r1.area();
+}
