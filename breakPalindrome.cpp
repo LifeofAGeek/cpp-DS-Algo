610ace0335c557620ff28462c1c60cdf29d3a6c0
LifeofAGeek <sethi8001@gmail.com>
2020-02-06 03:37:40 +0530

    Break Palindrome - LeetCode

--- breakPalindrome.cpp
+++ breakPalindrome.cpp
@@ -1,8 +1,26 @@
 #include<bits/stdc++.h>
+#include<string>
 using namespace std;
 
-Class Solution
+class Solution
 {
 public:
-    string breakPalindrome
+    string breakPalindrome(string s)
+    {
+        int n=s.length();
+        if(n==1){return " ";}
+        for(int i=0;i<n/2;i++){
+            if(s[i]!='a'){
+                s[i]='a';
+                return s;
+            }
+        }
+        s[n-1]='b';
+        return s;
+    }
+};
+
+int main(){
+Solution answer;
+cout<<answer.breakPalindrome("abba");
 }
