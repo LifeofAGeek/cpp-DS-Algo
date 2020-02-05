217deb34860496c414d21a942c92bd158d26d39f
LifeofAGeek <sethi8001@gmail.com>
2020-02-06 01:49:24 +0530

    Smart Pointers

--- smartPointers.cpp
+++ smartPointers.cpp
@@ -9,6 +9,7 @@ private:
     float breadth;
 
 public:
+
     Rectangle(float l=0, float b=0) //parametrized constructor
     {
         length=l;
@@ -21,3 +22,9 @@ public:
     }
 
 };
+
+int main()
+{
+    unique_ptr<Rectangle> p(new Rectangle(10,5));
+    cout<<p->area()<<endl;
+}
