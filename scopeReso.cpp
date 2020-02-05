f5e4d4931fd79a92823603ca2a72e206e8351c1f
LifeofAGeek <sethi8001@gmail.com>
2020-02-05 23:20:18 +0530

    Scope Resolution Operator

---
+++ scopeReso.cpp
@@ -0,0 +1,63 @@
+class Rectangle
+{
+private:
+int length;
+int breadth;
+public:
+Rectangle();
+Rectangle(int l,int b);
+Rectangle(Rectangle &r);
+int getLength(){return length;}
+int getBreadth(){return breadth;}
+void setLength(int l);
+void setBreadth(int b);
+int area();
+int perimeter();
+bool isSquare();
+~Rectangle();
+};
+int main()
+{
+Rectangle r1(10,10);
+cout<<"Area "<<r1.area()<<endl;
+if(r1.isSquare())
+cout<<"Yes"<<endl;
+}
+Rectangle::Rectangle()
+{
+length=1;
+breadth=1;
+}
+Rectangle::Rectangle(int l,int b)
+{
+length=l;
+breadth=b;
+}
+Rectangle::Rectangle(Rectangle &r)
+{
+length=r.length;
+breadth=r.breadth;
+}
+void Rectangle::setLength(int l)
+{
+length=l;
+}
+void Rectangle::setBreadth(int b)
+{
+breadth=b;
+}
+int Rectangle::area()
+{
+return length*breadth;
+}
+int Rectangle::perimeter()
+{
+return 2*(length+breadth);
+}
+bool Rectangle::isSquare()
+{
+return length==breadth;
+}
+Rectangle::~Rectangle(){
+cout<<"Rectangle Destroyed";
+}
