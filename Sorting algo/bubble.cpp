3e3733d6c41dab6b9bb30678d95c066c2f0b5dba
LifeofAGeek <sethi8001@gmail.com>
2020-02-06 05:13:29 +0530

    bubble sorting

--- Sorting algo/bubble.cpp
+++ Sorting algo/bubble.cpp
@@ -1,21 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int BubbleSort(int A[],int n){
-for(int j=0;j<=n;j++){
-    for(int i=0;i<n-2;i++){
+int BubbleSort(int A[],int n,int c=0){
+int flag;
+for(int j=1;j<=n;j++){
+    flag=0;
+    c=c+1;
+    for(int i=0;i<n-1;i++){
         if(A[i]<A[i+1]){
             int temp=A[i];
             A[i]=A[i+1];
             A[i+1]=temp;
+            flag=1;
+
         }
     }
+    if(flag==0){
+            cout<<"Number of pass is "<<c<<" out of "<<n<<endl;
+            return 0;}
 }
 }
 
 int main()
 {
-    int A[10]={2,5,31,22,9,74,91,11,32,7};
+    int A[]={2,5,31,22,9,74,91,11,32,7,87,65,40,44,86,91,63};
     int n=sizeof(A)/sizeof(A[0]);
     BubbleSort(A,n);
     for(int i=0;i<n;i++)
