4411d833cf04458e43e3e6a4683586ae97fde77e
LifeofAGeek <sethi8001@gmail.com>
2020-02-08 04:33:04 +0530

    codechef cook-off

--- Sorting algo/CHNGIT.cpp
+++ Sorting algo/CHNGIT.cpp
@@ -1,22 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int CHNGIT(int arr[], int n)
+{
+     unordered_map<int, int> dict;
+     for(int i=0;i<n;i++)
+        dict[arr[i]]++;
+     int max_count=0;
+     for(auto i:dict)
+     {
+         if(max_count<i.second){
+                max_count=i.second;
+         }
+     }
+     return(n-max_count);
+}
+
 int main()
 {
-    int testCase; //test case input
-    cin>>testCase;
-    while(testCase--)
-    {
-        int n; //size of integer array
-        int arr[n]; //sequence of integers
-        int freq[101]={0}; //frequency of each element
-        for(int i=0;i<n;i++)
-        {
-            cin>>arr[i];
-            freq[arr[i]]++;
-        }
-    int c=0;
-    sort(freq,freq+101);
-    cout<<n-freq[100]<<endl;
-}
+    int A[]={10, 20, 10, 20, 30, 20, 20};
+    int n=sizeof(A)/sizeof(A[0]);
+    cout<<"No. of moves required is:"<<CHNGIT(A,n);
 }
