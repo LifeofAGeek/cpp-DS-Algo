417057eebe3ccb0c570d8d923b51086ab2138975
LifeofAGeek <sethi8001@gmail.com>
2020-02-08 01:01:18 +0530

    Merge sorting

--- Sorting algo/merge.cpp
+++ Sorting algo/merge.cpp
@@ -1,1 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int display(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<endl;
+}
+
+int MergeSort(int A[], int n)
+{
+    if(n==1)
+        return;
+
+    int mid=n/2,left[mid],right[n-mid];
+
+    for(int i=0;i<mid;i++)
+        left[i]=A[i];
+
+}
+
+int Merge(int L[], int R[], int A[])
+{
+    int nL=sizeof(L)/sizeof(L[0]);
+    int nR=sizeof(R)/sizeof(R[0]);
+    int n=sizeof(A)/sizeof(A[0]);
+    int i=0,j=0,k=0;
+
+    while(i<nL && j<nR)
+    {
+        if(L[i]<=R[j])
+            A[k]=L[i];
+            i++;
+        else
+            A[k]=R[j];
+            j++;
+        k++;
+    }
+
+    while(i<nL)
+        A[k]=L[i];
+        k++;
+        i++;
+
+    while(j<=nR)
+        A[k]=R[j];
+        k++;
+        j++
+}
+
+int main()
+{
+    int A[]={2,5,31,22,9,74,11,32,7,87,65,40,44,86,91,63};
+    int n=sizeof(A)/sizeof(A[0]);
+    InsertionSort(A,n);
+    display(A,n);
+}
 
