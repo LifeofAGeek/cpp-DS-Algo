49b96573874f4b25a5f89600a4c5d0aa72b297bb
LifeofAGeek <sethi8001@gmail.com>
2020-02-07 22:38:43 +0530

    Merge sorting

---
+++ Sorting algo/merge.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MergeSort
+{
+public:
+    int A[];
+
+    MergeSort()
+
+    sorting(int A[], int n)
+    {
+
+    }
+};
+
+int main()
+{
+
+}
