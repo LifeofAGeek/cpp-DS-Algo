0418613e569958d6475bc1182171ace7b814b7cd
LifeofAGeek <sethi8001@gmail.com>
2020-02-07 23:25:53 +0530

    Insertion sorting

--- Sorting algo/insertion.cpp
+++ Sorting algo/insertion.cpp
@@ -1,47 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class InserttionSort
+void swaping(int *xp, int *yp)
 {
-public:
-    int A[];
-    int n;
+int temp = *xp;
+*xp = *yp;
+*yp = temp;
+}
 
-    InserttionSort(int x[], int len)
+void sorting(int A[], int n)
+{
+    int value,hole;
+    for(int i=1;i<n;i++)
     {
-        n=len;
-        A=x[n];
-    }
-
-    void swaping(int *xp, int *yp)
-    {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-    }
-
-    void sorting()
-    {
-        int value,hole;
-        for(int i=1;i<n;i++)
+        value=A[i];
+        hole=i;
+        while(hole>0 && A[hole-1]>value)
         {
-            value=A[i];
-            hole=i;
-            while(hole>0 && A[hole-1]>value)
-            {
-                swaping(&A[hole],&A[hole-1]);
-                hole--;
-            }
-            A[hole]=value;
+            swaping(&A[hole],&A[hole-1]);
+            hole--;
         }
-
+        A[hole]=value;
     }
-};
+
+}
 
 int main()
 {
     int A[]={2,5,31,22,9,74,11,32,7,87,65,40,44,86,91,63};
     int n=sizeof(A)/sizeof(A[0]);
-    InserttionSort arr(A,n);
+
 }
 
