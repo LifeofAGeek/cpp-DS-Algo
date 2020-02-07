b58f1d2d87c307c9f7d0eab5f0487e0d066b8e8d
LifeofAGeek <sethi8001@gmail.com>
2020-02-08 01:53:13 +0530

    Quick sort

--- Sorting algo/Quick.cpp
+++ Sorting algo/Quick.cpp
@@ -1,9 +1,34 @@
-#include<iostream>
-#include<stdlib.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int Partition(int *A,int start, int en)
+int Partition(int *A,int start, int End)
+{
+    int pivot=A[End];
+    int pIndex=start;
+    for(int i=start;i<=End-1;i++)
+    {
+        if(A[i]<=pivot)
+        {
+            swap(A[i],A[pIndex]);
+            pIndex++;
+        }
+    }
+    swap(A[pIndex],A[End]);
+    return pIndex;
+}
+
+void QuickSort(int *A, int start, int End)
+{
+    if(start<End)
+    {
+        int Pindex=Partition(A,start,End);
+        QuickSort(A,start,Pindex-1);
+        QuickSort(A,Pindex+1,End);
+    }
+}
+
+int main()
 {
 
-
 }
+
