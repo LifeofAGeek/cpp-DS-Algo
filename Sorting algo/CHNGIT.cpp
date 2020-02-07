e90b7d7cbc6cdd5719575ef716617268f58cef6e
LifeofAGeek <sethi8001@gmail.com>
2020-02-08 04:21:18 +0530

    codechef cook-off

--- Sorting algo/CHNGIT.cpp
+++ Sorting algo/CHNGIT.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int testCase; //test case input
+    cin>>testCase;
+    while(testCase--)
+    {
+        int n; //size of integer array
+        int arr[n]; //sequence of integers
+        int freq[101]={0}; //frequency of each element
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+            freq[arr[i]]++;
+        }
+    int c=0;
+    sort(freq,freq+101);
+    cout<<n-freq[100]<<endl;
+}
+}
