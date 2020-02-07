6e34600883c451c965c928b0b1289258acef4750
LifeofAGeek <sethi8001@gmail.com>
2020-02-07 22:59:35 +0530

    Insertion sorting

---
+++ Sorting algo/insertion.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class InserttionSort
+{
+public:
+    int A[];
+
+    InserttionSort(int x[])
+    {
+        A=x;
+    }
+
+    void sorting()
+    {
+        int n=sizeof(A)/sizeof(A[0]);
+
+    }
+};
+
+int main()
+{
+
+}
+
