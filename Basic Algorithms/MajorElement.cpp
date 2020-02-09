a02148f576f6b34759185e7f10761a0f9931ebe4
LifeofAGeek <sethi8001@gmail.com>
2020-02-09 23:59:48 +0530

    Moore's Voting Algorith

--- Basic Algorithms/MajorElement.cpp
+++ Basic Algorithms/MajorElement.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int findMajority(int arr[], int n)
+{
+    int major=0, c=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[major]==arr[i]) c++;
+        else
+            c--;
+        if(c==0)
+            major=i;
+    }
+}
+
+bool isMajor(int major, int arr[], int n)
+{
+    for(int i)
+}
