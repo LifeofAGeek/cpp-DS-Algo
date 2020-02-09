ac18cd19a435c7ad8ef109736f3135b1e9bbc948
LifeofAGeek <sethi8001@gmail.com>
2020-02-10 00:52:21 +0530

    Kadane's Algorithm

--- Basic Algorithms/MaxSumSubarray.cpp
+++ Basic Algorithms/MaxSumSubarray.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int bruteForce(int arr[], int n)
+int bruteForce_n_cube(int arr[], int n)
 {
     int max_sum=INT_MIN;
     for(int i=0;i<n;i++){
@@ -17,9 +17,21 @@ int bruteForce(int arr[], int n)
     return max_sum;
 }
 
+int bruteForce_n_square(int arr[], int n)
+{
+    int max_sum=INT_MIN, sum;
+    for(int i=0;i<n;i++){ sum=0;
+        for(int j=i;j<n;j++){
+            sum+=arr[j];
+            if(sum>max_sum)
+                max_sum=sum;
+        }
+    }
+    return max_sum;
+}
 int main()
 {
     int a[]={1,2,3,4};
     int n=(sizeof(a))/sizeof(a[0]);
-    cout<<"Max Sum Subarray is= "<<bruteForce(a,n);
+    cout<<"Max Sum Subarray is= "<<bruteForce_n_square(a,n);
 }
