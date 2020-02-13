9ac5f19a5e6fca5ae79250a9c7f7708f228650aa
LifeofAGeek <sethi8001@gmail.com>
2020-02-14 00:11:10 +0530

    To arrange -ve number one side and positive on the other side

--- Data Structures in C++/arrayADT6.cpp
+++ Data Structures in C++/arrayADT6.cpp
@@ -1,19 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Array
-{
-int A[10];
-int size;
-int length;
-};
-
-void Display(struct Array arr)
+void Display(int arr[], int n)
 {
 int i;
 cout<<"Elements are"<<endl;
-for(i=0;i<arr.length;i++)
-    cout<<arr.A[i];
+for(i=0;i<n;i++)
+    cout<<arr[i];
 }
 
-
+int arrange(int arr[], int n)
+{
+    int i=0,j=n-1;
+}
