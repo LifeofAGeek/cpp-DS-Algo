f353202dd318f148fccf2ec90abb74a1db198d8b
LifeofAGeek <sethi8001@gmail.com>
2020-02-14 01:55:24 +0530

    Linkedlist basic

--- Data Structures in C++/linkedList3.cpp
+++ Data Structures in C++/linkedList3.cpp
@@ -12,7 +12,18 @@ void createNode(int arr[], int n)
 {
     int i;
     Node *t,*last;
-    head=
+    head=new Node;
+    head->data=arr[0];
+    head->next=nullptr;
+    last=head;
+
+    for(i=1;i<n;i++)
+    {
+        t=new Node;
+        t->data=arr[i];
+        t
+    }
+
 }
 
 void Display(struct Node *p)
