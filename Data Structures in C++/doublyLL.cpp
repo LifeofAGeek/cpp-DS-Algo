da063a349918e885f22dda5d3e3feed2b8d8d05a
LifeofAGeek <sethi8001@gmail.com>
2020-02-21 01:29:44 +0530

    Added func. to create a nodes in Circular LL

---
+++ Data Structures in C++/doublyLL.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int value;
+    Node *next;
+    Node *prev;
+};
+Node *first=nullptr;
+
+void create_nodes(int arr[], int n)
+{
+    Node *t,*last;
+    first=new Node;
+    first->value=arr[0];
+    first->prev=first->next=nullptr;
+    last=first;
+
+    for(int i=1;i<n;i++)
+    {
+        t=new Node;
+        t->value=arr[i];
+        last->next=t;
+        t->next=nullptr;
+        last=t;
+    }
+}
+
+void display()
+{
+    Node *p=first;
+    while(p)
+    {
+        cout<<p->value<<" ";
+        p=p->next;
+    }
+}
+
+int main()
+{
+    int a[]={1,2,1,3,4,5};
+    create_node(a,6);
+    display(head);
+    cout<<endl;
+
+}
