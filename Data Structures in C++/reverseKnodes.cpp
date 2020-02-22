c63bfab4f1a4d77d10a21934c3b59fde22e64e7c
LifeofAGeek <sethi8001@gmail.com>
2020-02-22 19:25:03 +0530

    To reverse only first k-nodes of the LL

--- Data Structures in C++/reverseKnodes.cpp
+++ Data Structures in C++/reverseKnodes.cpp
@@ -6,12 +6,41 @@ struct Node
     int value;
     Node *next;
 };
-Node *head;
+Node *head=nullptr;
+
+void createNode(int arr[], int n) //array data to link list data
+{
+    int i;
+    Node *t,*last;
+    head=new Node;
+    head->value=arr[0];
+    head->next=nullptr;
+    last=head;
+
+    for(i=1;i<n;i++)
+    {
+        t=new Node;
+        t->value=arr[i];
+        t->next=nullptr;
+        last->next=t;
+        last=t;
+    }
+
+}
+
+void Display(struct Node *p) // front to end
+{   cout<<"Displaying LinkedList:"<<endl;
+    while(p!=0)
+    {
+        cout<<p->value<<" ";
+        p=p->next;
+    }
+}
 
 void reverse_K_nodes(int index)
 {
-    Node *q=nullptr,*r=nullptr,*p,*temp=head,*join;
-    for(int i=0;i<index;i++)
+    Node *q=nullptr,*r=nullptr,*p=head,*temp=head,*join;
+    for(int i=0;i<index-1;i++)
         temp=temp->next;
     join=temp->next;
     temp->next=nullptr;
@@ -21,5 +50,21 @@ void reverse_K_nodes(int index)
         q=p;
         p=p->next;
         q->next=r;
+    }head=q;
+    temp=head;
+    while(temp->next!=0)
+    {
+        temp=temp->next;
     }
+    temp->next=join;
+}
+
+int main()
+{
+    int a[]={1,2,6,3,4,5};
+    createNode(a,6);
+    Display(head);
+    cout<<endl;
+    reverse_K_nodes(3);
+    Display(head);
 }
