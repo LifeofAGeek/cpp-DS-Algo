f6c806d743e2dbc301c4e12944e4d627dd234b13
LifeofAGeek <sethi8001@gmail.com>
2020-03-10 19:58:32 +0530

    Queue implimentation using Array

--- Data Structures in C++/Queue/queue1.cpp
+++ Data Structures in C++/Queue/queue1.cpp
@@ -1,4 +1,73 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+class Queue
+{
+private:
+    int size;
+    int front;
+    int rear;
+    int *Q;
+    bool isEmpty()
+    {
+        return (rear == front) ? true : false;
+    }
+    bool isFull()
+    {
+        return (rear == size - 1) ? true : false;
+    }
 
+public:
+    Queue(int size)
+    {
+        this->size = size;
+        Q = new int[size];
+        front = rear = -1;
+    }
+
+    void enqueue(int x);
+    int dequeue();
+    void display();
+};
+
+void Queue::enqueue(int x)
+{
+    if (isFull())
+        cout << "Queue is Full";
+    else
+    {
+        q[++rear] = x;
+    }
+}
+
+int Queue::dequeue()
+{
+    int x = -1;
+    if (isEmpty())
+        printf("Queue is Empty\n");
+    else
+    {
+        x = Q[front + 1];
+        front++;
+    }
+    return x;
+}
+
+void Queue::display()
+{
+    for (int i = front + 1; i <= rear; i++)
+        printf("%d ", Q[i]);
+    printf("\n");
+}
+
+int main()
+{
+    Queue q(5);
+    
+    q.enqueue(1);
+    q.enqueue(9);
+    q.enqueue(9);
+    q.enqueue(7);
+
+
+}
