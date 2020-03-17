a829357661ebda3fd4fc0a60218fa07574fed053
LifeofAGeek <sethi8001@gmail.com>
2020-03-18 02:33:03 +0530

    Queue implimentation using LL

---
+++ Data Structures in C++/Queue/queue2.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x) : data(x), next(nullptr) {}
+};
+
+class Queue
+{
+private:
+    Node *front, *rear;
+    int size;
+
+public:
+    Queue()
+    {
+        size=0;
+        front=rear=nullptr;
+    }
+
+    bool isEmpty()
+    {
+        return (front==nullptr?true:false);
+    }
+
+    void enqueue(int x);
+    int dequeue();
+    void display();
+};
+
+void Queue::enqueue(int x)
+{
+    Node *temp=new Node(x);
+    if(temp==nullptr) cout<<"Queue is Full"<<endl;
+    else
+    {
+        if(front==nullptr)
+        {
+            front=rear=temp;
+        }
+        else
+        {
+            rear->next=temp;
+            rear=temp;
+        }
+    }
+    size++;
+}
+
+int Queue::dequeue()
+{
+    int x=-1;
+    Node *t;
+    if(!isEmpty())
+    {
+        t=front;
+        x=t->data;
+        front=front->next;
+        delete(t);
+    }
+    else
+    {
+        cout<<"Queue is Empty"<<endl;
+    }
+    return x;
+}
+
+void Queue::display()
+{
+    Node *p=front;
+    while (p)
+    {
+        cout<<p->data<<endl;
+        p=p->next;
+    }
+}
+
+int
