936473fdc43f2078dcf4cd9b408e3e47bd816cc0
LifeofAGeek <sethi8001@gmail.com>
2020-03-19 01:46:30 +0530

    BST Insert, search and Delete

--- Data Structures in C++/BST/BST.cpp
+++ Data Structures in C++/BST/BST.cpp
@@ -1,20 +1,19 @@
-// BST Insert and Search
+// BST Insert, search and Delete
 #include <stdio.h>
 #include <stdlib.h>
 struct Node
 {
-    Node *lchild;
+    struct Node *lchild;
     int data;
-    Node *rchild;
+    struct Node *rchild;
 } *root = NULL;
-
 void Insert(int key)
 {
-    Node *t = root;
-    Node *r = NULL, *p;
+    struct Node *t = root;
+    struct Node *r = NULL, *p;
     if (root == NULL)
     {
-        p = new Node;
+        p = (struct Node *)malloc(sizeof(struct Node));
         p->data = key;
         p->lchild = p->rchild = NULL;
         root = p;
@@ -61,6 +60,76 @@ struct Node *Search(int key)
     }
     return NULL;
 }
+struct Node *RInsert(struct Node *p, int key)
+{
+    struct Node *t = NULL;
+    if (p == NULL)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = key;
+        t->lchild = t->rchild = NULL;
+        return t;
+    }
+    if (key < p->data)
+        p->lchild = RInsert(p->lchild, key);
+    else if (key > p->data)
+        p->rchild = RInsert(p->rchild, key);
+    return p;
+}
+int Height(struct Node *p)
+{
+    int x, y;
+    if (p == NULL)
+        return 0;
+    x = Height(p->lchild);
+    y = Height(p->rchild);
+    return x > y ? x + 1 : y + 1;
+}
+struct Node *InPre(struct Node *p)
+{
+    while (p && p->rchild != NULL)
+        p = p->rchild;
+    return p;
+}
+struct Node *InSucc(struct Node *p)
+{
+    while (p && p->lchild != NULL)
+        p = p->lchild;
+    return p;
+}
+struct Node *Delete(struct Node *p, int key)
+{
+    struct Node *q;
+    if (p == NULL)
+        return NULL;
+    if (p->lchild == NULL && p->rchild == NULL)
+    {
+        if (p == root)
+            root = NULL;
+        free(p);
+        return NULL;
+    }
+    if (key < p->data)
+        p->lchild = Delete(p->lchild, key);
+    else if (key > p->data)
+        p->rchild = Delete(p->rchild, key);
+    else
+    {
+        if (Height(p->lchild) > Height(p->rchild))
+        {
+            q = InPre(p->lchild);
+            p->data = q->data;
+            p->lchild = Delete(p->lchild, q->data);
+        }
+        else
+        {
+            q = InSucc(p->rchild);
+            p->data = q->data;
+            p->rchild = Delete(p->rchild, q->data);
+        }
+    }
+    return p;
+}
 int main()
 {
     struct Node *temp;
@@ -72,7 +141,7 @@ int main()
     Delete(root, 30);
     Inorder(root);
     printf("\n");
-    temp = Search(20);
+    temp = Search(60);
     if (temp != NULL)
         printf("element %d is found\n", temp->data);
     else
