fa9a331ca06909b29e1aaeb115b92c44113377c9
LifeofAGeek <sethi8001@gmail.com>
2020-03-19 01:10:45 +0530

    BST

--- Data Structures in C++/BST/BST.cpp
+++ Data Structures in C++/BST/BST.cpp
@@ -61,3 +61,46 @@ TreeNode *Queue::dequeue()
     return x;
 }
 
+class Tree
+{
+public:
+    Tree() { root = NULL; }
+    void CreateTree();
+    void Inorder(TreeNode *p);
+};
+
+void Tree::CreateTree()
+{
+    TreeNode *p, *t;
+    int x;
+    printf("Eneter root value ");
+    scanf("%d", &x);
+    root = new TreeNode;
+    root->data = x;
+    root->lchild = root->rchild = NULL;
+    q.enqueue(root);
+    while (!q.isEmpty())
+    {
+        p = q.dequeue();
+        printf("eneter left child of %d :", p->data);
+        scanf("%d", &x);
+        if (x != -1)
+        {
+            t = new TreeNode;
+            t->data = x;
+            t->lchild = t->rchild = NULL;
+            p->lchild = t;
+            q.enqueue(t);
+        }
+        printf("eneter right child of %d :", p->data);
+        scanf("%d", &x);
+        if (x != -1)
+        {
+            t = new TreeNode;
+            t->data = x;
+            t->lchild = t->rchild = NULL;
+            p->rchild = t;
+            q.enqueue(t);
+        }
+    }
+}
