4f8e90b2b90214c444f82507846d5c0654d4ef9d
LifeofAGeek <sethi8001@gmail.com>
2020-05-14 13:16:00 +0530

    Number theory

---
+++ CP/Number_theory.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//primality test is to determine whether the input integer is a prime num or not.
+bool primality_test(int n)
+{
+    /* brute force approach is O(n) i.e.,
+    if(n==1) return false;
+    for (int i = 2; i < n; i++)
+    {
+        if(n%i==0) return false;
+    }
+    return true; */
+
+    /* better approach -> all divisor of a number occurs in a pair of 
+    
+    
+    */
+}
+
+int main()
+{
+    cout<<primality_test()<<endl;
+}
