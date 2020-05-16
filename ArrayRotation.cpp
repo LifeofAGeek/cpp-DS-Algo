b92e4a2ba53c55eb62ae544cbdc296c94a2871c5
Anubhav Sethi <32430170+LifeofAGeek@users.noreply.github.com>
2020-05-16 22:40:28 +0530

    Array Rotation

---
+++ ArrayRotation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;//size of original array
+    int arr[n];
+    for(int& a:arr) cin>>a;
+    int temp[n]; 
+    int i=0,d;
+    cin>>d;
+    while(i<=5){ //left rotation by d in O(n) time 
+        if(d%6==0) d%=6;
+        temp[i]=arr[d];
+        if(d==(n-1-d)) break;
+        i++, d++;
+    }
+    for(int x : temp) cout<<x<<" "; //left rotated array
+    return 0;
+}
