e9fbfefe5a3857c978e12bfdc8862e2c0a7003e3
LifeofAGeek <sethi8001@gmail.com>
2020-07-03 13:31:07 +0530

    fixed bugs

--- CP/GraphTheory_CP.cpp
+++ CP/GraphTheory_CP.cpp
@@ -1,52 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int v, vector<bool>& vis, vector<int> g[]){
+vector<int> g[1001]; //undirected graph, not zero based indexing
+vector<bool> vis(1001,false); //to keep track of visited nodes
+vector<int> dist(1001,0); //distance array of every node from source node
+int N,M; // N = No. of Nodes , M = No. of Edges
+
+template <class T>
+void print(vector<T> arr) //print array of T type
+{
+    for(int i=1;i<=N;i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
+
+void clear(){
+   for(int i=1;i<=N;i++) vis[i]=false;
+   for(int i=1;i<=N;i++) dist[i]=0;
+}
+
+void dfs(int v){
     vis[v]=true;
     cout<<v<<" ";
     for(int u:g[v]){
-        if(!vis[u]) dfs(u,vis,g);
+        if(!vis[u]) dfs(u);
     }
 }
 
-int countCC(vector<int> g[], vector<bool> vis) //count conected components
+int countCC(int N) //count conected components
 {
     int c=0;
-    for (int i = 1; i < vis.size(); i++)
+    for (int i = 1; i <= N; i++)
     {
         if(!vis[i]){
-            dfs(i,vis,g), ++c;
+            dfs(i), ++c;
         }
     }
     return c;
 }
 
-void SSSP(vector<int> g[], vector<bool> &vis, vector<int> &dist, int s, int l=0) //Single Source Shortest Path(On Tree)
+void SSSP(int s, int l=0) //Single Source Shortest Path(On Tree)
 { 
     vis[s]=true;
     dist[s]=l;
     for(int u:g[s]){
-        if(!vis[u]) SSSP(g, vis, dist , u, dist[s]+1);
+        if(!vis[u]) SSSP(u, dist[s]+1);
     }
 
 }
 
 int main()
 {
-    int N,M; // N = No. of Nodes , M = No. of Edges
     cin>>N>>M;
     //take graph input using Adj. list
-    vector<int> g[N+1]; //undirected graph, not zero based indexing
     while(M--){
         int u,v;
         cin>>u>>v;
         g[u].push_back(v);
         g[v].push_back(u);
     }
-    vector<bool> vis(N+1,false);
-    vector<int> dist(N+1,0);
-    SSSP(g,vis,dist,2,0);
-    for(int i=1;i<=N;i++) cout<<dist[i]<<" ";
-    //cout<<countCC(g,vis)<<endl;
-    //dfs(1,vis,g);
+    
+    
+    SSSP(1,0);
+    print(dist);
+    cout<<endl;
+    //cout<<"No. of Connected components are "<<countCC(N)<<endl;
+    clear();
+    dfs(1);
 }
