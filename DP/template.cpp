1dd6c757988481569349a6af4b9cb6cd8432e3a4
LifeofAGeek <sethi8001@gmail.com>
2020-08-31 23:37:11 +0530

    basic template

--- DP/template.cpp
+++ DP/template.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 #define ld long double
 #define int long long
-const int m =1e9+7;
+const int mod =1e9+7;
+const int mex =1e5+1;
+int dp[mex][mex];
 
 void solve(){
     //code goes here
